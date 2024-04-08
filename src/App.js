71402c4ef024eff8cc58bc1e999036d8b373f90d
omidi.afsaneh <omidi.afsaneh@golpakhsheaval.com>
2024-04-08 19:18:48 +0330

    add back

--- src/App.js
+++ src/App.js
@@ -34,8 +34,9 @@ const App = () => {
       // پیدا کردن دوربین باکیفیت پشت
       const rearCameraId = cameras.find(
         (camera) =>
-          camera.label.toLowerCase().includes("back") &&
-          camera.label.toLowerCase().includes("0")
+          (camera.label.toLowerCase().includes("back") &&
+            camera.label.toLowerCase().includes("0")) ||
+          camera.label.toLowerCase().includes("back")
       )?.deviceId;
 
       setCameraId(rearCameraId);
