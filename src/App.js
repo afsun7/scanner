2a8c666b78be62de9120cab4b68d89aa9b890fbf
omidi.afsaneh <omidi.afsaneh@golpakhsheaval.com>
2024-04-08 19:03:19 +0330

    index 1

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ const App = () => {
           camera.label.toLowerCase().includes("back")
       )?.deviceId;
 
-      setCameraId(camerasFilter);
+      setCameraId(camerasFilter[1]);
       return cameras;
     };
     enableCamera()
